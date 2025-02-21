2adc36b128d29c87ec0bdb3a3e41112b5c64a0c3
niki0410 <nikiboni13@gmail.com>
2025-02-21 10:39:07 +0100

    fix logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.png';
-import logo2 from './logo2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="appHeader">
-        <img src={logo2} className="img-fluid appLogo" alt="logo" />
+        <img src={logo} className="img-fluid appLogo" alt="logo" />
       </header>
     </div>
   );
