edf2e7223906a1d83c404cd30090ecffc40b592a
niki0410 <nikiboni13@gmail.com>
2025-02-21 10:35:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import logo2 from './logo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="appHeader">
+        <img src={logo2} className="img-fluid appLogo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
