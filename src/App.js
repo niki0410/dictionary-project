a99c008914a846ad97664b5872b92ace9ca879d9
niki0410 <nikiboni13@gmail.com>
2025-03-23 18:40:10 +0100

    links

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <main>
         <Dictionary defaultKeyword="book" />
       </main>
-      <footer className='appFooter'><small>Coded by SheCodes</small></footer>
+      <footer className='appFooter'><small>Coded by Nikolett Hartmann and is open-sourced on <a href='https://github.com/niki0410/dictionary-project' target='_blank' rel="noreferrer">GitHub</a> and hosted on <a href='https://dictionary-app-niki.netlify.app/' target='_blank' rel="noreferrer">Netlify</a></small></footer>
     </div>
     </div> 
   );
