f8d4e8c24cd6581397c864a464fc00cde85349a0
niki0410 <nikiboni13@gmail.com>
2025-03-23 12:07:49 +0100

    synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,19 +1,70 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-    console.log(props)
+  console.log(props.meaning.synonyms);
+  if(props.meaning.definition && !props.meaning.example && props.meaning.synonyms){
+    return(
+      <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <p>
+          <strong>Definition:</strong>
+          {props.meaning.definition}
+          <br />
+          <strong>Synonyms:</strong>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+      </div>
+    </div>
+    )
+  }
+  if(props.meaning.definition && props.meaning.example && !props.meaning.synonyms){
+    return(
+      <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <p>
+          <strong>Definition:</strong>
+          {props.meaning.definition}
+          <br />
+          <strong>Example:</strong>
+          <em>{props.meaning.example}</em>
+        </p>
+      </div>
+    </div>
+    )
+  }
+  if(props.meaning.definition && !props.meaning.example && !props.meaning.synonyms){
+    return(
+      <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <p>
+          <strong>Definition:</strong>
+          {props.meaning.definition}
+        </p>
+      </div>
+    </div>
+    )
+  }
+  if(props.meaning.definition && props.meaning.example && props.meaning.synonyms){
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      
-          <div >
-            <p>
-              {props.meaning.definition}
-              <br />
-              <em>{props.meaning.example}</em>
-            </p>
-          </div>
-        
+      <div>
+        <p>
+          <strong>Definition:</strong>
+          {props.meaning.definition}
+          <br />
+          <strong>Example:</strong>
+          <em>{props.meaning.example}</em>
+          <br />
+          <strong>Synonyms:</strong>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+      </div>
     </div>
   );
+}
 }
